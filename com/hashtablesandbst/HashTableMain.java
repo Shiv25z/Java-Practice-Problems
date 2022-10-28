573636f41806d4c097877c7e9d8130b6c5b9d613
Shiv25z <shivamzagade25899@gmail.com>
2022-10-28 12:49:48 +0530

    Added: Day 15 Assignment Problem -HashTable and BST Problems

---
+++ com/hashtablesandbst/HashTableMain.java
@@ -0,0 +1,36 @@
+package com.hashtablesandbst;
+
+import java.util.Scanner;
+
+public class HashTableMain {
+    public static void main(String[] args) {
+        //HashTableExp Object for Generic Type
+        MyMapNode<String> myMapNode = new MyMapNode<>();
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+
+        String strArr[] = str.split(" ");
+
+        System.out.println("Given String ="+str);
+        //Passing Array Element in HashTable Index
+        for(String mystr : strArr) {
+            //Get index of each object
+            int index = myMapNode.myHashFunction(mystr);
+            //Store Element in Hash Table at given index
+            myMapNode.storeKey(index,mystr);
+        }
+
+        //Methode to Show Hash Table Data
+        myMapNode.showHashList();
+
+        //Code to Remove the Given Word from HashTable Linked List
+        System.out.println("\nEnter the Word to Remove from Hash Table :");
+        String word = sc.next();
+        int index = myMapNode.myHashFunction(word);
+        myMapNode.removeKey(index,word);
+
+        myMapNode.showHashList();
+    }
+}
